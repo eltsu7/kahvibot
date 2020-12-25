85423c6306521d70e9ae325fbc7142243a05831d
eltsu7 <eeli.hernesniemi@gmail.com>
2020-12-25 23:51:40 +0200

    update view names

--- kahvidb.sql
+++ kahvidb.sql
@@ -12,13 +12,13 @@ create table nimet (
     username    varchar(255)    not null
 );
 -- näkymät
-create view juonnit_ja_nimet as
+create view kahvit as
     select id, update_id, username, aika, kuvaus
     from juonnit
     inner join nimet on juonnit.user_id = nimet.user_id
     order by id desc;
 
-create view juontilaskuri as
+create view kuppilaskuri as
     select nimet.user_id, username, count(*) as kupit
     from juonnit
     inner join nimet on juonnit.user_id = nimet.user_id
